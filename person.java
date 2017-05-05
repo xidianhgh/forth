cb965c6e7e413f46f4e0e6a7dc2ff028bf63f8ba
your name <your email>
2017-05-05 09:35:54 +0800

    first commit

---
+++ person.java
@@ -0,0 +1,29 @@
+//Source file: E:\\ND\\temp\\person.java
+
+
+private class person 
+{
+   
+   /**
+    * 对人的编号
+    */
+   private Integer id;
+   private String name;
+   
+   /**
+    * @roseuid 58F47E9C01DD
+    */
+   public person() 
+   {
+    
+   }
+   
+   /**
+    * 设置ID号
+    * @roseuid 58F366390323
+    */
+   public void setID() 
+   {
+    
+   }
+}
---
+++ young.java
@@ -0,0 +1,14 @@
+//Source file: E:\\ND\\temp\\young.java
+
+
+public class young extends person 
+{
+   
+   /**
+    * @roseuid 58F47E9C01FE
+    */
+   public young() 
+   {
+    
+   }
+}
